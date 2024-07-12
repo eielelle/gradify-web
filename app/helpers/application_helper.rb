a2e524e32d30d22d5d154064e3c540768a59c996
eielelle <eielelle915@gmail.com>
2024-07-12 20:39:07 +0800

    added highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def border_class(condition)
     condition ? 'border-b border-x' : ''
   end
+
+  def current_drawer?(path)
+    'bg-primary text-white rounded-lg' if current_page?(path)
+  end
 end
