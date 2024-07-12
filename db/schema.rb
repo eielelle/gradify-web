d2e4f77defc80737e70e6e921b1db809c4ea59d5
eielelle <eielelle915@gmail.com>
2024-07-12 17:00:11 +0800

    added paper trail gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_17_053435) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_12_085510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_17_053435) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_student_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_student_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.bigint "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object"
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "admin_accounts", "permissions"
