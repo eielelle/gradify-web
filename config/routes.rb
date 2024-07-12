d6f8cdfd12ea503d6fd36eeadd8fabe3be908838
eielelle <eielelle915@gmail.com>
2024-07-12 19:23:46 +0800

    configured redirect after sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :student_accounts
   # TODO: fix route scope later
+  devise_for :student_accounts
   devise_for :admin_accounts, path: :admin #, only: [:sessions]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,10 +12,11 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "welcome#index"
+  get "/admin", to: "admin/overview#index", as: :admin_account_root
+
   # get "/admin/dashboard", to: "admin_panel#index"
   namespace :admin do
-    resources :overview, only: [:index]
     resources :admins do
       get 'export', on: :collection
       get 'send_exports', on: :collection
