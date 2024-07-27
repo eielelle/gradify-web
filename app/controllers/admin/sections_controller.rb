76c42849de70d1fdf3679687b0fd743345a1d764
Jmsmr <mjamesmar@gmail.com>
2024-07-27 22:32:36 +0800

    History Section

--- app/controllers/admin/sections_controller.rb
+++ app/controllers/admin/sections_controller.rb
@@ -5,6 +5,8 @@ module Admin
     before_action :set_section, only: %i[show edit update destroy archive]
     before_action :set_search, only: %i[index new create edit update]
     include ExportableFormatConcern
+    include SearchableConcern
+    include SnapshotConcern
     def index
       # Your index method code here
       @sections = @q.result(distinct: true).page(params[:page]).per(10)
@@ -67,6 +69,39 @@ module Admin
 
     def send_exports
       send_format params
+    end
+
+    def history
+      set_default_sort(default_sort_column: 'created_at desc')
+      @q = PaperTrail::Version.ransack(params[:q])
+      @items = @q.result(distinct: true).where(item_type: 'Section').page(params[:page]).per(10)
+      @count = @items.count
+      @sort_fields = get_sort_fields(PaperTrail::Version)
+      query_items_history(PaperTrail::Version, params, model_name: 'Section')
+    end
+
+    def version 
+      set_default_sort(default_sort_column: 'created_at desc')
+      @q = PaperTrail::Version.ransack(params[:q])
+      @items = @q.result(distinct: true).where(item_id: params[:id] || params.dig(:q, :id)).page(params[:page]).per(10)
+      @count = @items.count
+      @sort_fields = get_sort_fields(PaperTrail::Version)
+    end
+
+    def snapshot
+      @version = PaperTrail::Version.find(params[:id])
+      @section = get_snapshot(@version)
+    end
+
+    def rollback
+      @version = PaperTrail::Version.find(params[:id])
+      @section = get_snapshot(@version)
+      
+      if @section.save(validate: false)
+        redirect_to admin_sections_versions_path(id: @version.item_id)
+      else
+        flash[:toast] = 'Rollback Unsuccessful'
+      end
     end
 
     private
