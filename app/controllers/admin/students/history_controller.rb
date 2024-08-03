40cd3745b2582616c3d92b5baf2227823be171ff
KaiKidThief <roxas.269269@meycauayan.sti.edu.ph>
2024-08-03 18:27:04 +0800

    fix historyController

--- app/controllers/admin/students/history_controller.rb
+++ app/controllers/admin/students/history_controller.rb
@@ -18,11 +18,12 @@ module Admin
       def versions
         set_default_sort(default_sort_column: 'created_at desc')
         @q = PaperTrail::Version.ransack(params[:q])
-        @results = @q.result(distinct: true).where(item_id: params[:id] || params.dig(:q, :id), item_type: 'StudentAccount')
+        @results = @q.result(distinct: true).where(item_id: params[:id] || params.dig(:q, :id),
+                                                   item_type: 'StudentAccount')
         @items = @results.page(params[:page]).per(10)
         @count = @items.count
         @sort_fields = get_sort_fields(PaperTrail::Version)
-      end      
+      end
 
       def snapshot
         @version = PaperTrail::Version.find(params[:id])
