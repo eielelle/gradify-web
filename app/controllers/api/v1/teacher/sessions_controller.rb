b33aecb3e4b5398f49da2d02f82b2645128147d5
eielelle <eielelle915@gmail.com>
2024-08-06 13:24:50 +0800

    rubocop

--- app/controllers/api/v1/teacher/sessions_controller.rb
+++ app/controllers/api/v1/teacher/sessions_controller.rb
@@ -1,39 +1,66 @@
 # frozen_string_literal: true
 
-class Api::V1::Teacher::SessionsController < Devise::SessionsController
-  protect_from_forgery with: :null_session
-  respond_to :json
-  private
+module Api
+  module V1
+    module Teacher
+      class SessionsController < Devise::SessionsController
+        protect_from_forgery with: :null_session
+        respond_to :json
 
-  def respond_with(resource, _opts = {})
-    
-    render json: {
-      status: {code: 200, message: 'Logged in sucessfully.'},
-      data: TeacherSerializer.new(resource).serializable_hash[:data][:attributes]
-    }, status: :ok
-  end
+        private
 
-  def respond_to_on_destroy
-    puts current_teacher_account.inspect
-    puts current_admin_account.inspect
-    if current_teacher_account
-      render json: {
-        status: 200,
-        message: "logged out successfully"
-      }, status: :ok
-    else
-      render json: {
-        status: 401,
-        message: "Couldn't find an active session."
-      }, status: :unauthorized
+        def respond_with(resource, _opts = {})
+          if resource.id.nil?
+            log_in_fail
+          else
+            log_in_success
+          end
+        end
+
+        def respond_to_on_destroy
+          if current_teacher_account
+            log_out_success
+          else
+            log_out_fail
+          end
+        end
+
+        def log_in_success
+          render json: {
+            status: { code: 200, message: 'Logged in sucessfully.' },
+            data: TeacherSerializer.new(resource).serializable_hash[:data][:attributes]
+          }, status: :ok
+        end
+
+        def log_in_fail
+          render json: {
+            status: { code: 401, message: 'Incorrect email or password' },
+            errors: ['Incorrect email or password']
+          }, status: :unauthorized
+        end
+
+        def log_out_success
+          render json: {
+            status: 200,
+            message: 'Logged out successfully'
+          }, status: :ok
+        end
+
+        def log_out_fail
+          render json: {
+            status: 401,
+            message: "Couldn't find an active session."
+          }, status: :unauthorized
+        end
+        # before_action :configure_sign_in_params, only: [:create]
+
+        # protected
+
+        # If you have extra params to permit, append them to the sanitizer.
+        # def configure_sign_in_params
+        #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+        # end
+      end
     end
   end
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
--- app/serializers/teacher_serializer.rb
+++ app/serializers/teacher_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class TeacherSerializer
   include JSONAPI::Serializer
   attributes :id, :email, :created_at
