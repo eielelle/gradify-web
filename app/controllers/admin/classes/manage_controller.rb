f310578e6dcf8003fad2d6553fc85acf4746c1bc
eielelle <eielelle915@gmail.com>
2024-08-14 08:36:44 +0800

    edit class

--- app/controllers/admin/classes/manage_controller.rb
+++ app/controllers/admin/classes/manage_controller.rb
@@ -22,9 +22,48 @@ class Admin::Classes::ManageController < Admin::LayoutController
         end
       end
 
+      def edit
+        set_class
+
+        redirect_to admin_classes_manage_index_path if @school_class.nil?
+      end
+
+      def update
+        set_class
+
+        account_not_found and return if @school_class.nil?
+
+        if @school_class.update(update_class_params[:school_class])
+          update_success
+          return
+        else
+          handle_errors(@school_class)
+        end
+
+        render :edit, status: :unprocessable_entity
+      end
+
       private
+
+      def update_class_params
+        params.permit(:id, school_class: %i[name description])
+      end
 
       def class_params
         params.permit(:name, :description)
+      end
+
+      def update_success
+        flash[:toast] = 'Updated Successfully.'
+        redirect_to admin_classes_manage_index_path
+      end
+
+      def account_not_found
+        flash[:notice] = 'Account not found.'
+        render :edit, status: :unprocessable_entity
+      end
+
+      def set_class
+        @school_class = SchoolClass.find(params[:id])
       end
 end
