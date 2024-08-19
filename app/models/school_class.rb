55a108e0d03304481334ce316da3719ab85aab4b
eielelle <eielelle915@gmail.com>
2024-08-20 05:25:30 +0800

    reduced sort options

--- app/models/school_class.rb
+++ app/models/school_class.rb
@@ -38,7 +38,7 @@ class SchoolClass < ApplicationRecord
   end
 
   def self.ransackable_attributes(_auth_object = nil)
-    get_export_fields(%i[description])
+    %w[name created_at]
   end
 
   # Allowlist associations for Ransack
