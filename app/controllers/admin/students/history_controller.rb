400a2f4543c2a02d8d7b76ce2d8eef2c2fbc3618
eielelle <eielelle915@gmail.com>
2024-08-31 22:14:35 +0800

    fix rollback error in teacher and student

--- app/controllers/admin/students/history_controller.rb
+++ app/controllers/admin/students/history_controller.rb
@@ -3,17 +3,8 @@
 module Admin
   module Students
     class HistoryController < Admin::LayoutController
-      include SnapshotConcern
       include SearchableConcern
-      include SortConcern
-
-      def index
-        set_default_sort(default_sort_column: 'created_at desc')
-        @q = PaperTrail::Version.ransack(params[:q])
-        @items = @q.result(distinct: true).where(item_type: 'StudentAccount').page(params[:page]).per(10)
-        @count = @items.count
-        @sort_fields = get_sort_fields(PaperTrail::Version)
-      end
+      include SnapshotConcern
 
       def versions
         set_default_sort(default_sort_column: 'created_at desc')
@@ -26,21 +17,32 @@ module Admin
       end
 
       def snapshot
-        @version = PaperTrail::Version.find(params[:id])
-        @student = get_snapshot(@version)
+        find_version_and_snapshot
       end
 
       def rollback
-        @version = PaperTrail::Version.find(params[:id])
+        find_version_and_snapshot
 
-        @student = @version.reify
+        PaperTrail.request.whodunnit = current_admin_account.name
+        @student.paper_trail_event = 'rollback'
 
         if @student.save(validate: false)
-          redirect_to admin_students_versions_path(id: @version.item_id), notice: 'Rollback successful.'
+          redirect_to admin_students_versions_path(id: @version.item_id)
         else
           flash[:toast] = 'Rollback Unsuccessful'
-          render :snapshot
         end
+      end
+
+      def index
+        set_default_sort(default_sort_column: 'created_at desc')
+        query_items_history(PaperTrail::Version, params, model_name: 'StudentAccount')
+      end
+
+      private
+
+      def find_version_and_snapshot
+        @version = PaperTrail::Version.find(params[:id])
+        @student = get_snapshot(@version)
       end
     end
   end
