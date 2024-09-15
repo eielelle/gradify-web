677625fdd8a18ac3f2bb530c558ab208d5ac9b2c
eielelle <eielelle915@gmail.com>
2024-09-15 18:19:54 +0800

    refactor edit password

--- app/controllers/admin/workforce/password_controller.rb
+++ app/controllers/admin/workforce/password_controller.rb
@@ -7,13 +7,14 @@ module Admin
       include PasswordConcern
       include SuperAdminConcern
 
+      before_action :set_user
+
       def edit
-        @user = User.find_by(id: params[:id])
         redirect_to admin_workforce_manage_index_path if @user.nil? || @user.role == 'superadmin'
       end
 
       def update
-        return if superadmin_redirect(User.find(params[:id]), edit_admin_workforce_password_path,
+        return if superadmin_redirect(User.find(params[:id]), edit_admin_workforce_manage_path(@user),
                                       'Cannot change password of SuperAdmin')
 
         update_model_password resource_class: User
@@ -21,12 +22,16 @@ module Admin
 
       private
 
+      def set_user
+        @user = User.find_by(id: params[:id])
+      end
+
       def edit_password_path
-        edit_admin_workforce_password_path
+        edit_admin_workforce_manage_path(@user, hide: false)
       end
 
       def after_update_path
-        admin_workforce_manage_index_path
+        edit_admin_workforce_manage_path(@user, hide: true)
       end
     end
   end
