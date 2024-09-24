cdb1f29668dfe607b5c2aa82690435ff1a907274
eielelle <eielelle915@gmail.com>
2024-09-24 21:47:29 +0800

    fix

--- app/controllers/admin/exams/export_controller.rb
+++ app/controllers/admin/exams/export_controller.rb
@@ -8,7 +8,7 @@ module Admin
       include ExportableFormatConcern
 
       def index
-        @exam_fields = Exam.get_export_fields(%i[encrypted_password reset_password_token id created_at])
+        @exam_fields = Exam.get_export_fields()
       end
 
       def download
