881042fa7cd3fc793e3a30e520df8976f1919cfe
KaiKidThief <roxas.269269@meycauayan.sti.edu.ph>
2024-10-18 09:37:27 +0800

    ADD EXAM OVERVIEW

--- app/controllers/student/exams/manage_controller.rb
+++ app/controllers/student/exams/manage_controller.rb
@@ -11,6 +11,23 @@ module Student
         end
   
         def show
+          @student = current_user
+          @exam = Exam.find(params[:id])
+          @subject = @exam.subject
+  
+          # Fetch exam results for the student
+          # Assuming you have an Exams Result model that belongs to both Exam and User
+          #@exam_result = @exam.exam_results.find_by(user: @student)
+  
+          #if @exam_result.nil?
+            #flash.now[:alert] = "No exam results found for this exam."
+          #else
+            #@total_score = @exam_result.total_score
+            #@correct_answers = @exam_result.correct_answers
+            #@incorrect_answers = @exam_result.incorrect_answers
+            #@double_answers = @exam_result.double_answers
+           # @no_answers = @exam_result.no_answers
+         # end
         end
   
   
