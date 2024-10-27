84549cf7c6fefdf80fe87334869b5fa0739acbb1
eielelle <eielelle915@gmail.com>
2024-10-27 11:26:32 +0800

    added exam list api

---
+++ app/controllers/api/v1/teacher/exams_controller.rb
@@ -0,0 +1,54 @@
+module Api
+    module V1
+        module Teacher
+            class ExamsController < ApplicationController
+                respond_to :json
+
+                def index
+                    teacher = current_user
+                    
+                    if current_user.nil?
+                        render json: unauthorized, status: :unauthorized
+                    else
+                        subject = teacher.subjects.all
+                        exams = Exam.where(subject_id: subject)
+                        puts "HEY"
+                        puts exams.length
+    
+                        render json: {
+                            exams: ExamSerializer.new(exams).serializable_hash[:data]
+                        }
+                    end
+                end
+
+                private
+
+                def unauthorized
+                    return {
+                        status: 401,
+                        message: "Unauthorized",
+                        errors: [
+                            "Not currently logged in"
+                        ]
+                    }
+                end
+
+                def serialize_exam(exams)
+                    n = exams.map do |exam|
+                        quarter = Quarter.find_by(id: exam[:quarter_id])
+                        subject = Subject.find_by(id: exam[:subject_id])
+
+                        return {
+                            exam:,
+                            quarter:,
+                            subject:
+                        }
+                    end
+
+                    puts "N LENGTH"
+                    puts n.length
+                end
+            end
+        end
+    end
+end
