dee4ac14d04858fe89a58772c007696fb28e79c8
eielelle <eielelle915@gmail.com>
2024-11-09 06:44:45 +0800

    added graph

--- app/controllers/admin/overview_controller.rb
+++ app/controllers/admin/overview_controller.rb
@@ -7,6 +7,14 @@ module Admin
       @total_teachers = User.where(role: 'teacher').count
       @total_admins = User.where(role: ['admin', 'superadmin']).count
       @total_classes = SchoolClass.where(params[:id]).count
+
+      # Define an array to hold the count of students per month
+      @studs = (1..12).map do |month|
+        # Get the count of students for the given month and year (e.g., for 2024)
+        User.where(role: 'student')
+            .where(created_at: Date.new(Time.current.year, month, 1).beginning_of_month..Date.new(2024, month, 1).end_of_month)
+            .count
+      end
     end
   end
 end
