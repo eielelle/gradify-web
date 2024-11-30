b84e6162a8c28343eb6c6deda09b1d4ebf3d2455
eielelle <eielelle915@gmail.com>
2024-12-01 00:58:20 +0800

    removed smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
     # Dafault email mailer
-    config.action_mailer.default_url_options = { host: 'gradify.onrender.com' }
+    # config.action_mailer.default_url_options = { host: 'gradify.onrender.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
